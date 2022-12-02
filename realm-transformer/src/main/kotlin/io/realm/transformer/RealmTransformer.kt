fa1a27933c4c26eb217a7218e3c02743b259b179
Pavlo Stavytskyi <pstavytskyi-turo>
2022-12-02 08:28:04 -0800

    Restored RealmAnalytics back

--- realm-transformer/src/main/kotlin/io/realm/transformer/RealmTransformer.kt
+++ realm-transformer/src/main/kotlin/io/realm/transformer/RealmTransformer.kt
@@ -18,17 +18,21 @@ package io.realm.transformer
 
 import com.android.build.api.variant.AndroidComponentsExtension
 import com.android.build.gradle.internal.publishing.AndroidArtifacts
+import io.realm.analytics.RealmAnalytics
 import io.realm.transformer.build.BuildTemplate
 import io.realm.transformer.build.FullBuild
-import io.realm.transformer.ext.getBootClasspath
+import io.realm.transformer.ext.*
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
+import org.gradle.api.artifacts.ResolvedArtifact
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.Directory
 import org.gradle.api.file.RegularFile
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.ListProperty
+import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Classpath
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputFiles
 import org.gradle.api.tasks.TaskAction
@@ -47,18 +51,37 @@ val READ_TIMEOUT = 2000L;
 
 // Wrapper for storing data from org.gradle.api.Project as we cannot store a class variable to it
 // as that conflict with the Configuration Cache.
-data class ProjectMetaData(val bootClassPath: Set<File>)
+data class ProjectMetaData(
+        val bootClassPath: Set<File>,
+        val usesKotlin: Boolean,
+        val targetType: String,
+        val targetSdk: String,
+        val minSdk: String,
+        val agpVersion: String,
+        val appId: String,
+        val gradleVersion: String,
+        val usesSync: Boolean,
+        val isGradleOffline: Boolean) {
+}
 
 /**
  * This class implements the Transform API provided by the Android Gradle plugin.
  */
-class RealmTransformer(bootClasspath: ConfigurableFileCollection,
+class RealmTransformer(private val metadata: ProjectMetaData,
                        private val inputs: ListProperty<Directory>,
                        private val allJars: ListProperty<RegularFile>,
                        private val referencedInputs: ConfigurableFileCollection,
                        private val output: RegularFileProperty) {
 
-    private val metadata = ProjectMetaData(bootClassPath = bootClasspath.files)
+    private val analytics: RealmAnalytics? = try {
+        val analytics = RealmAnalytics()
+        analytics.calculateAnalyticsData(metadata)
+        analytics
+    } catch (e: Exception) {
+        // Analytics should never crash the build.
+        logger.debug("Could not calculate Realm analytics data:\n$e")
+        null
+    }
 
     companion object {
 
@@ -78,6 +101,15 @@ class RealmTransformer(bootClasspath: ConfigurableFileCollection,
                                 )
                             }.files)
                             it.bootClasspath.setFrom(project.getBootClasspath())
+                            it.offline.set(project.gradle.startParameter.isOffline)
+                            it.targetType.set(project.targetType())
+                            it.usesKotlin.set(project.usesKotlin())
+                            it.minSdk.set(project.getMinSdk())
+                            it.targetSdk.set(project.getTargetSdk())
+                            it.agpVersion.set(project.getAgpVersion())
+                            it.usesSync.set(Utils.isSyncEnabled(project))
+                            it.gradleVersion.set(project.gradle.gradleVersion)
+                            it.appId.set(project.getAppId())
                         }
                     component.artifacts.forScope(com.android.build.api.variant.ScopedArtifacts.Scope.PROJECT)
                         .use<ModifyClassesTask>(taskProvider)
@@ -89,6 +121,29 @@ class RealmTransformer(bootClasspath: ConfigurableFileCollection,
                         )
                 }
             }
+        }
+
+        private fun Project.targetType(): String = with(project.plugins) {
+            when {
+                findPlugin("com.android.application") != null -> "app"
+                findPlugin("com.android.library") != null -> "library"
+                else -> "unknown"
+            }
+        }
+
+        private fun Project.usesKotlin(): Boolean {
+            for (conf in project.configurations) {
+                try {
+                    for (artifact: ResolvedArtifact in conf.resolvedConfiguration.resolvedArtifacts) {
+                        if (artifact.name.startsWith("kotlin-stdlib")) {
+                            return true
+                        }
+                    }
+                } catch (ignore: Exception) {
+                    // Some artifacts might not be able to resolve, in this case, just ignore them.
+                }
+            }
+            return false
         }
     }
 
@@ -140,10 +195,12 @@ class RealmTransformer(bootClasspath: ConfigurableFileCollection,
 
     private fun exitTransform(timer: Stopwatch) {
         timer.stop()
+        analytics?.execute()
     }
 }
 
 abstract class ModifyClassesTask: DefaultTask() {
+
     @get:Classpath
     abstract val fullRuntimeClasspath : ConfigurableFileCollection
 
@@ -156,12 +213,57 @@ abstract class ModifyClassesTask: DefaultTask() {
     @get:InputFiles
     abstract val bootClasspath: ConfigurableFileCollection
 
+    @get:Input
+    abstract val targetType: Property<String>
+
+    @get:Input
+    abstract val offline: Property<Boolean>
+
+    @get:Input
+    abstract val usesKotlin: Property<Boolean>
+
+    @get:Input
+    abstract val targetSdk: Property<String>
+
+    @get:Input
+    abstract val minSdk: Property<String>
+
+    @get:Input
+    abstract val agpVersion: Property<String>
+
+    @get:Input
+    abstract val appId: Property<String>
+
+    @get:Input
+    abstract val gradleVersion: Property<String>
+
+    @get:Input
+    abstract val usesSync: Property<Boolean>
+
     @get:OutputFiles
     abstract val output: RegularFileProperty
 
     @TaskAction
     fun taskAction() {
-        RealmTransformer(bootClasspath, allDirectories, allJars, fullRuntimeClasspath, output).transform()
+        val metadata = ProjectMetaData(
+                bootClassPath = bootClasspath.files,
+                usesKotlin = usesKotlin.get(),
+                targetType = targetType.get(),
+                targetSdk = targetSdk.get(),
+                minSdk = minSdk.get(),
+                agpVersion = agpVersion.get(),
+                appId = appId.get(),
+                gradleVersion = gradleVersion.get(),
+                usesSync = usesSync.get(),
+                isGradleOffline = offline.get()
+        )
+        RealmTransformer(
+                metadata = metadata,
+                inputs = allDirectories,
+                allJars = allJars,
+                referencedInputs = fullRuntimeClasspath,
+                output = output
+        ).transform()
     }
 }
 
